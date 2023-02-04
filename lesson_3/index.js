53392fb3ae411e8ac7b7a2cfb31e365a10f2c9ee
Gagik <gags.rap@gmail.com>
2023-02-04 19:07:39 +0400

    lesson_3

--- lesson_3/index.js
+++ lesson_3/index.js
@@ -29,39 +29,37 @@
 //     }
 // })
 const takeAShower = () => {
-    console.log('take a shower ');
-}
+  console.log("take a shower ");
+};
 const makeCoffee = () => {
-    throw new Error('coffee machine does not work')
-    console.log('coffee was made')
-}
+  throw new Error("coffee machine does not work");
+  console.log("coffee was made");
+};
 const getReadyForWork = () => {
+  console.log(true);
+};
+// const isReadyForWork =
 
-    console.log(true)
-}
-// const isReadyForWork = 
-
-let isLoading = true
+let isLoading = true;
 const happyDay = async () => {
-    console.log('loading....');
-    Promise.resolve('waked up')
-        // .then('want to sleep more')
-        .then((responseComingFromResolveMessage) => {
-            console.log(responseComingFromResolveMessage)
-            console.log('loading 10%....');
-        })
-        .then((responseFromPreviousResolvedFunction) => takeAShower(responseFromPreviousResolvedFunction))
-        .then(makeCoffee)
-        .then(getReadyForWork)
-        .catch(console.warn)
-        .finally(() => {
-            isLoading = false
-        })
-}
-happyDay()
+  console.log("loading....");
+  Promise.resolve("waked up")
+    // .then('want to sleep more')
+    .then((responseComingFromResolveMessage) => {
+      console.log(responseComingFromResolveMessage);
+      console.log("loading 10%....");
+    })
+    .then((responseFromPreviousResolvedFunction) =>
+      takeAShower(responseFromPreviousResolvedFunction)
+    )
+    .then(makeCoffee)
+    .then(getReadyForWork)
+    .catch(console.warn)
+    .finally(() => {
+      isLoading = false;
+    });
+};
+happyDay();
 setTimeout(() => {
-    console.log(isLoading);
-}, 0)
-
-console.log("hello");
-console.log("hello");
+  console.log(isLoading);
+}, 0);
