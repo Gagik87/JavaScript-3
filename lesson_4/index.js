0c9e28216b40ce7dca7178820fa44dcc356a85b2
Gagik <gags.rap@gmail.com>
2023-02-04 21:29:41 +0400

    lesson_4

--- lesson_4/index.js
+++ lesson_4/index.js
@@ -1,1 +1,17 @@
-console.log("Lesson_4");
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", () => {
+  console.log("requested.....");
+  const applyForVisa = new Promise((resolve, reject) => {
+    if (Math.random() * 10 > 5) {
+      resolve("resolved");
+    } else {
+      reject("rejected");
+    }
+  });
+  setTimeout(() => {
+    applyForVisa
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  }, 2000);
+});
